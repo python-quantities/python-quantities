f87ab99f3124236aa7d500cface12e87ee7281c8
Darren Dale <darren.dale@cornell.edu>
2009-01-14 12:38:55 -0500

    updated acceleration

--- quantities/units/acceleration.py
+++ quantities/units/acceleration.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 """
 
@@ -5,8 +6,11 @@ from quantities.units.unitquantity import UnitQuantity
 from quantities.units.time import s
 from quantities.units.length import m
 
-g = gravity = force = free_fall = standard_free_fall = gp = dynamic = \
-    geopotential = \
-    UnitQuantity('g', 9.806650*m/s**2) # TODO: check
+g = gravity = standard_gravity = gee = force = free_fall = \
+    standard_free_fall = gp = dynamic = geopotential = UnitQuantity(
+    'standard_gravity',
+    9.806650*m/s**2,
+    symbol='g₀'
+) # exact
 
 del m, s, UnitQuantity
