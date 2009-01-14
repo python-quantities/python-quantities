6fe62f4a18560fb8ca967510bb1b7c96ecd1705c
Darren Dale <darren.dale@cornell.edu>
2009-01-14 09:10:46 -0500

    updated time.py

--- quantities/units/time.py
+++ quantities/units/time.py
@@ -1,75 +1,184 @@
+# -*- coding: utf-8 -*-
 """
 """
 
 from quantities.units.unitquantity import UnitQuantity, UnitTime
 
 
-s = sec = second = seconds = \
-    UnitTime('s')
-ms = millisecond = milliseconds = \
-    UnitTime('ms', s/1000)
-us = microsecond = microseconds = \
-    UnitTime('us', ms/1000)
-ns = nanosecond = nanoseconds = \
-    UnitTime('ns', us/1000)
-ps = picosecond = picoseconds = \
-    UnitTime('ps', ns/1000)
-fs = femtosecond = femtoseconds = \
-    UnitTime('fs', ps/1000)
-attosecond = attoseconds = \
-    UnitTime('attosecond', fs/1000) # as is a keyword in python2.6
-jiffy = jiffies = \
-    UnitTime('jiffy', s/100) # no kidding?
+s = sec = second = UnitTime(
+    'second',
+    symbol='s',
+    aliases=['sec', 'seconds']
+)
+ms = millisecond = UnitTime(
+    'millisecond',
+    s/1000,
+    'ms',
+    aliases=['milliseconds']
+)
+us = microsecond = UnitTime(
+    'microsecond',
+    ms/1000,
+    symbol='µs',
+    aliased=['us', 'microseconds']
+)
+ns = nanosecond = UnitTime(
+    'nanosecond',
+    us/1000,
+    symbol='ns',
+    aliases=['nanoseconds']
+)
+ps = picosecond = UnitTime(
+    'picosecond',
+    ns/1000,
+    symbol='ps',
+    aliases=['picoseconds']
+)
+fs = femtosecond = UnitTime(
+    'femtosecond',
+    ps/1000,
+    symbol='fs',
+    aliases=['femtoseconds']
+)
+attosecond = UnitTime(
+    'attosecond',
+    fs/1000,
+    symbol='as',
+    aliases=['attoseconds']
+) # as is a keyword in python2.6
+jiffy = UnitTime(
+    'jiffy',
+    s/100,
+    aliases=['jiffies']
+) # no kidding?
 
-min = minute = minutes = \
-    UnitTime('min', 60*s)
-h = hr = hour = hours = \
-    UnitTime('hr', 60*min)
-d = day = days = \
-    UnitTime('day', 24*hr)
-week = weeks = \
-    UnitTime('week', 7*day)
-fortnight = \
-    UnitTime('fortnight', 2*weeks)
-yr = year = years = tropical_year = tropical_years = a = \
-    UnitTime('yr', 3.15569259747e7*s) # a for anno
-month = \
-    UnitTime('month', yr/12)
-shake = shakes = \
-    UnitTime('shake', 1e-8*s)
-sidereal_second = sidereal_seconds = \
-    UnitTime('sidereal_second', 0.9972696*s)
-sidereal_minute = sidereal_minutes = \
-    UnitTime('sidereal_minute', 60*sidereal_second)
-sidereal_hour = sidereal_hours = \
-    UnitTime('sidereal_hour', 60*sidereal_minute)
-sidereal_day = sidereal_days = \
-    UnitTime('sidereal_day', 24*sidereal_hour)
-sidereal_year = sidereal_years = \
-    UnitTime('sidereal_year', 1.00003878*year)
-sidereal_month = sidereal_months = \
-    UnitTime('sidereal_month', 27.321661*day)
-tropical_month = tropical_months = \
-    UnitTime('tropical_month', 27.321582*day)
-lunar_month = lunar_months = \
-    UnitTime('lunar_month', 29.530589*day)
-common_year = common_years = \
-    UnitTime('common_year', 365*day)
-leap_year = leap_years = \
-    UnitTime('leap_year', 366*day)
-Julian_year = Julian_years = \
-    UnitTime('Julian_year', 365.25*day)
-Gregorian_year = Gregorian_years = \
-    UnitTime('Gregorian_year', 365.2425*day)
+min = minute = UnitTime(
+    'minute',
+    60*s,
+    symbol='min',
+    aliases=['minutes']
+)
+h = hr = hour = UnitTime(
+    'hour',
+    60*min,
+    symbol='h',
+    aliases=['hr', 'hours']
+)
+d = day = UnitTime(
+    'day',
+    24*hr,
+    symbol='d',
+    aliases=['days']
+)
+week = UnitTime(
+    'week',
+    7*day,
+    aliases=['weeks']
+)
+fortnight = UnitTime(
+    'fortnight',
+    2*weeks,
+    aliases=['fortnights']
+)
+yr = year = tropical_year = a = UnitTime(
+    'year',
+    3.15569259747e7*s,
+    symbol='yr',
+    aliases=['a', 'years', 'tropical_year', 'tropical_years']
+) # a for anno
+month = UnitTime(
+    'month',
+    yr/12,
+    aliases=['months']
+)
+shake = UnitTime(
+    'shake',
+    1e-8*s,
+    aliases=['shakes']
+)
 
-millenium = millenia = \
-    UnitTime('eon', 1000*year)
-eon = eons = \
-    UnitTime('eon', 1e9*year)
+sidereal_day = UnitTime(
+    'sidereal_day',
+    day/1.00273790935079524,
+    aliases=['sidereal_days']
+) # approximate, see http://en.wikipedia.org/wiki/Sidereal_time
+sidereal_hour = UnitTime(
+    'sidereal_hour',
+    sidereal_day/24,
+    aliases=['sidereal_hours']
+)
+sidereal_minute = UnitTime(
+    'sidereal_minute',
+    sidereal_hour/60,
+    aliases=['sidereal_minutes']
+)
+sidereal_second = UnitTime(
+    'sidereal_second',
+    sidereal_minute/60,
+    aliases=['sidereal_seconds']
+)
+sidereal_year = UnitTime(
+    'sidereal_year',
+    366.25636042*sidereal_day,
+    aliases=['sidereal_years']
+) # http://en.wikipedia.org/wiki/Sidereal_year
+sidereal_month = UnitTime(
+    'sidereal_month',
+    27.321661*day,
+    aliases=['sidereal_months']
+) # http://en.wikipedia.org/wiki/Month#Sidereal_month
 
-work_year = \
-    UnitQuantity('work_year', 2056*hours)
-work_month = \
-    UnitQuantity('work_month', work_year/12)
+tropical_month = UnitTime(
+    'tropical_month',
+    27.321582*day,
+    aliases=['tropical_months']
+)
+synodic_month = lunar_month = UnitTime(
+    'synodic_month',
+    29.530589*day,
+    aliases=['synodic_months', 'lunar_month', 'lunar_months']
+) # long-term average, http://en.wikipedia.org/wiki/Month#Synodic_month
+common_year = UnitTime(
+    'common_year',
+    365*day,
+    aliases=['common_years']
+)
+leap_year = UnitTime(
+    'leap_year',
+    366*day,
+    aliases=['leap_years']
+)
+Julian_year = UnitTime(
+    'Julian_year',
+    365.25*day,
+    aliases=['Julian_years']
+)
+Gregorian_year = UnitTime(
+    'Gregorian_year',
+    365.2425*day,
+    aliases=['Gregorian_years']
+)
+
+millenium = UnitTime(
+    'millenium',
+    1000*year,
+    aliases=['millenia']
+)
+eon = UnitTime(
+    'eon',
+    1e9*year,
+    aliases=['eons']
+)
+
+work_year = UnitQuantity(
+    'work_year',
+    2056*hours,
+    aliases=['work_years']
+)
+work_month = UnitQuantity(
+    'work_month',
+    work_year/12,
+    aliases=['work_months']
+)
 
 del UnitQuantity
