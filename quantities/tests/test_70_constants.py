67f3c87c36f3ef4943188f5f5e7d2c702563b05b
Darren Dale <dsdale24@gmail.com>
2009-02-22 12:27:47 -0500

    added empty constants unit test file

---
+++ quantities/tests/test_70_constants.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from nose.tools import *
+from numpy.testing import *
+from numpy.testing.utils import *
+
+import numpy as np
+import quantities as q
+
