911119b2e2758a019ffe8899292f9ad3981aea1d
Tom Close <tclose@oist.jp>
2013-08-22 11:48:59 +0900

    Added molar concentrations

---
+++ quantities/units/concentration.py
@@ -0,0 +1,28 @@
+"""
+"""
+from __future__ import absolute_import
+
+from ..unitquantity import UnitQuantity
+from .substance import mol
+from .volume import L
+
+M = molar = UnitQuantity(
+    'molar',
+    mol / L,
+    symbol='M',
+    aliases=['molar', 'Molar']
+)
+
+mM = millimolar = UnitQuantity(
+    'millimolar',
+    molar / 1000,
+    symbol='mM',
+    aliases=['millimolar']
+)
+
+uM = micromolar = UnitQuantity(
+    'micromolar',
+    mM / 1000,
+    symbol='M',
+    aliases=['mircomolar']
+)
