0e3f10affe121e95d95211f458c0a002928328dd
Peter Konradi <codingchipmunk@posteo.net>
2022-12-06 14:06:37 +0100

    generated annotations for units

---
+++ quantities/units/area.pyi
@@ -0,0 +1,18 @@
+from ..unitquantity import UnitQuantity
+
+
+are: UnitQuantity
+ares: UnitQuantity
+b: UnitQuantity
+barn: UnitQuantity
+cmil: UnitQuantity
+circular_mil: UnitQuantity
+D: UnitQuantity
+darcy: UnitQuantity
+mD: UnitQuantity
+millidarcy: UnitQuantity
+ha: UnitQuantity
+hectare: UnitQuantity
+acre: UnitQuantity
+international_acre: UnitQuantity
+US_survey_acre: UnitQuantity
---
+++ quantities/units/velocity.pyi
@@ -0,0 +1,8 @@
+from ..unitquantity import UnitQuantity
+
+c: UnitQuantity
+speed_of_light: UnitQuantity
+kt: UnitQuantity
+knot: UnitQuantity
+knot_international: UnitQuantity
+international_knot: UnitQuantity
