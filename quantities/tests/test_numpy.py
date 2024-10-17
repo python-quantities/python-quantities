e27053e7f7c173bc8a202520b05e265a6ebb2154
zm711 <92116279+zm711@users.noreply.github.com>
2024-10-17 17:25:27 -0400

    fix test

--- quantities/tests/test_numpy.py
+++ quantities/tests/test_numpy.py
@@ -9,14 +9,14 @@ class TestNumpy2_0(TestCase):
     # issues between NumPy < 2.0 and > 2.0 appear to be missed in current quantities testing
     # https://github.com/NeuralEnsemble/python-neo/pull/1490
     # this is a small test class to add changes to ensure pre and post 2.0 compatability
-    
+
     def setUp(self):
         self.q = [[1, 2], [3, 4]] * pq.m
 
     def test_numpy_concatenate(self):
 
         concatenated_array = np.concatenate((self.q.flatten(), self.q.flatten()))
-        self.assertQuantityEqual(concatenated_array, [1,2,3,4]*pq.m)
+        self.assertQuantityEqual(concatenated_array, [1,2,3,4, 1,2,3,4]*pq.m)
     
     def test_numpy_dtype(self):
 
