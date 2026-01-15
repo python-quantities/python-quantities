Fix for failure to handle units when performing in-place multiplication and division with NumPy 2.x
The reason for the failure is that with NumPy 1.25, `super().__imul__(other)` calls `self.__array_prepare__(self, context)`,
but `_array_prepare__` was removed in NumPy 2.0.
Long-term, we should probably implement `__array_ufunc__`.
This inelegant fix will hopefully be enough to get a release out quickly, then we can revisit this when we have more time and fix it properly

quantities/quantity.py
@@ -16,6 +16,9 @@ PREFERRED = []  # List of preferred quantities for each symbol,
                 # e.g. PREFERRED = [pq.mV, pq.pA, pq.UnitQuantity('femtocoulomb', 1e-15*pq.C, 'fC')]
                 # Intended to be overwritten in down-stream packages
 
+_np_version = tuple(map(int, np.__version__.split(".dev")[0].split(".")))
+
+
 def validate_unit_quantity(value):
     try:
         assert isinstance(value, Quantity)
@@ -318,7 +321,7 @@ class Quantity(np.ndarray):
         return res
 
     def __array_wrap__(self, obj, context=None, return_scalar=False):
-        _np_version = tuple(map(int, np.__version__.split(".dev")[0].split(".")))
+
         # For NumPy < 2.0 we do old behavior
         if _np_version < (2, 0, 0):
             if not isinstance(obj, Quantity):
@@ -342,7 +345,6 @@ class Quantity(np.ndarray):
     @scale_other_units
     def __radd__(self, other):
         return np.add(other, self)
-        return super().__radd__(other)
 
     @with_doc(np.ndarray.__iadd__)
     @scale_other_units
@@ -358,7 +360,6 @@ class Quantity(np.ndarray):
     @scale_other_units
     def __rsub__(self, other):
         return np.subtract(other, self)
-        return super().__rsub__(other)
 
     @with_doc(np.ndarray.__isub__)
     @scale_other_units
@@ -378,22 +379,40 @@ class Quantity(np.ndarray):
     @with_doc(np.ndarray.__imul__)
     @protected_multiplication
     def __imul__(self, other):
-        return super().__imul__(other)
+        # the following is an inelegant fix for the removal of __array_prepare__ in NumPy 2.x
+        # the longer-term solution is probably to implement __array_ufunc__
+        # See:
+        # - https://numpy.org/devdocs/release/2.0.0-notes.html#array-prepare-is-removed
+        # - https://numpy.org/neps/nep-0013-ufunc-overrides.html
+        cself = self.copy()
+        cother = other.copy()
+        res = super().__imul__(other)
+        if _np_version < (2, 0, 0):
+            return res
+        else:
+            context = (np.multiply, (cself, cother, cself), 0)
+            return self.__array_prepare__(res, context=context)
 
     @with_doc(np.ndarray.__rmul__)
     def __rmul__(self, other):
         return np.multiply(other, self)
-        return super().__rmul__(other)
 
     @with_doc(np.ndarray.__itruediv__)
     @protected_multiplication
     def __itruediv__(self, other):
-        return super().__itruediv__(other)
+        # see comment above on __imul__
+        cself = self.copy()
+        cother = other.copy()
+        res = super().__itruediv__(other)
+        if _np_version < (2, 0, 0):
+            return res
+        else:
+            context = (np.true_divide, (cself, cother, cself), 0)
+            return self.__array_prepare__(res, context=context)
 
     @with_doc(np.ndarray.__rtruediv__)
     def __rtruediv__(self, other):
         return np.true_divide(other, self)
-        return super().__rtruediv__(other)
 
     @with_doc(np.ndarray.__pow__)
     @check_uniform
@@ -404,7 +423,15 @@ class Quantity(np.ndarray):
     @check_uniform
     @protected_power
     def __ipow__(self, other):
-        return super().__ipow__(other)
+        # see comment above on __imul__
+        cself = self.copy()
+        cother = other.copy()
+        res = super().__ipow__(other)
+        if _np_version < (2, 0, 0):
+            return res
+        else:
+            context = (np.power, (cself, cother, cself), 0)
+            return self.__array_prepare__(res, context=context)
 
     def __round__(self, decimals=0):
         return np.around(self, decimals)
@@ -528,7 +555,6 @@ class Quantity(np.ndarray):
 
     @with_doc(np.nansum)
     def nansum(self, axis=None, dtype=None, out=None):
-        import numpy as np
         return Quantity(
             np.nansum(self.magnitude, axis, dtype, out),
             self.dimensionality